bcec0daf578d098abf36de3cc2a0135eeb8cffb9
kabiryusufbashir <kabiryusufbashir@gmail.com>
2023-12-28 10:50:30 +0100

    Learnt about python print()

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,12 @@
-print('Hello World')
+#Pythton newline character
+print('This is PiccoloPay \nYour VTU plug')
+
+#Escaping newline character
+print('Hello, I\'m trying to escape the newline character')
+
+#Key word argument
+print('My name is', 'Yusuf,', end=' ')
+print('founder piccolopay')
+
+#Seperator 
+print('My', 'name', 'is', 'Yusuf', 'founder', 'of', 'piccolopay', sep="-")
